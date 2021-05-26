a63cd470a2ef292aed812d0aeb0cde548e0cb97d
Dawid <dawid.badurakrk@gmail.com>
2021-05-26 03:59:27 +0200

    add joke service

---
+++ src/main/java/guru/springframework/spring5jokesappv2/services/JokeSercviceImpl.java
@@ -0,0 +1,18 @@
+package guru.springframework.spring5jokesappv2.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeSercviceImpl implements JokeSercvice {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeSercviceImpl(){
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+    @Override
+    public String getJoke() {
+        return null;
+    }
+}
